6a9c824f85f7318f47603e841eb510382969635a
unknown <wanyi.ng1975@gmail.com>
2026-05-04 01:03:23 -0400

    update supabase client

--- src/client.js
+++ src/client.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js"
+import { config } from "dotenv"
 
-const URL = import.meta.env.VITE_SUPABASE_URL
-const API_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+config({ path: "../.env" })
+
+const URL = process.env.SUPABASE_URL
+const API_KEY = process.env.SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(URL, API_KEY)
