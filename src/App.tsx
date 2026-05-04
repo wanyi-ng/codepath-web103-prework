7ddec0b0b1e221f57496312a37d194bfc1d71342
unknown <wanyi.ng1975@gmail.com>
2026-05-04 01:05:02 -0400

    rename route name

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ import Home from './pages/Home'
 import ShowCreators from './pages/ShowCreators'
 import AddCreator from './pages/AddCreator'
 import ViewCreator from './pages/ViewCreator'
+import EditCreator from './pages/EditCreator'
 
 function App() {
 
     let element = useRoutes([
         { path: "/", element: <Home /> },
         { path: "/creators", element: <ShowCreators /> },
+        { path: "/creators/add", element: <AddCreator /> },
         { path: "/creators/:creatorId", element: <ViewCreator /> },
-        { path: "/add-creator", element: <AddCreator /> },
+        { path: "/creators/:creatorId/edit", element: <EditCreator /> },
     ])
 
     return element
